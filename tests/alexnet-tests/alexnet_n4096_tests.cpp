8b379f749d75c4eb46ee148e6994a6c6323b2fef
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2026-08-26 15:08:38 +0000

    alexnet: replace n4096 prog using belfortlabs cheddar backend for higher ciphertext size

--- tests/alexnet-tests/alexnet_n4096_tests.cpp
+++ tests/alexnet-tests/alexnet_n4096_tests.cpp
@@ -36,27 +36,38 @@
   using Enc = Encoder<word>;
 
 
-std::vector<Ct> alexnet_tiny(CtxPtr ctx, Enc& encoder, UI& ui, const std::vector<Ct>& v0);
-std::vector<Ct> alexnet_tiny__encrypt__arg0(CtxPtr ctx, Enc& encoder, UI& ui, const std::vector<double>& v0, UI& ui1);
-std::vector<double> alexnet_tiny__decrypt__result0(CtxPtr ctx, Enc& encoder, UI& ui, const std::vector<Ct>& v0, UI& ui1);
-std::tuple<CtxPtr, UI> __configure();
+void alexnet_tiny(Context<word>* v1, const Encoder<word>& v2, UserInterface<word>* v3, const EvaluationKey<word>& v4, const std::array<Ciphertext<word>, 1>& v5, std::array<Ciphertext<word>, 1>& v6);
+void alexnet_tiny__encrypt__arg0(Context<word>* v1, const Encoder<word>& v2, UserInterface<word>* v3, const EvaluationKey<word>& v4, float v5[1][3][16][16], UserInterface<word>* v6, std::array<Ciphertext<word>, 1>& v7);
+void alexnet_tiny__decrypt__result0(Context<word>* v1, const Encoder<word>& v2, UserInterface<word>* v3, const EvaluationKey<word>& v4, const std::array<Ciphertext<word>, 1>& v5, UserInterface<word>* v6, float* v7);
+void alexnet_tiny__configure(std::shared_ptr<Context<word>>& v1, std::unique_ptr<UserInterface<word>>& v2);
 
 TEST(AlexnetTiny, AlexNetN4096) {
-  auto[ctx, ui] = __configure();
+  std::shared_ptr<Context<word>> ctx;
+  std::unique_ptr<UserInterface<word>> ui;
+  alexnet_tiny__configure(ctx, ui);
+  const Evk& evk = ui->GetMultiplicationKey();
 
   std::filesystem::path inPath =
     std::filesystem::path(TEST_DATA_DIR) / "input.bin";
   std::ifstream in(inPath, std::ios::binary);
   std::vector<double> test_input(1 * 3 * 16 * 16);
   in.read(reinterpret_cast<char*>(test_input.data()), test_input.size() * sizeof(double));
+  std::vector<float> test_input_float(test_input.begin(), test_input.end());
+  float (*input_shaped)[3][16][16] =
+    reinterpret_cast<float (*)[3][16][16]>(test_input_float.data());
 
   // encrypt input
-  auto encrypted_input = alexnet_tiny__encrypt__arg0(ctx, ctx->encoder_, ui, test_input, ui);
+  std::array<Ciphertext<word>, 1> encrypted_input;
+  alexnet_tiny__encrypt__arg0(ctx.get(), ctx->encoder_, ui.get(), evk, input_shaped, ui.get(), encrypted_input);
+  std::cout << "Encrypted input size: " << encrypted_input.size() << std::endl;
   // alexnet_tiny on encrypted input
-  auto encrypted_result = alexnet_tiny(ctx, ctx->encoder_, ui, encrypted_input);
+  std::array<Ciphertext<word>, 1> encrypted_result;
+  alexnet_tiny(ctx.get(), ctx->encoder_, ui.get(), evk, encrypted_input, encrypted_result);
   // decrypt result
-  std::vector<double> result = alexnet_tiny__decrypt__result0(ctx, ctx->encoder_, ui, encrypted_result, ui);
+  std::vector<float> result(1 * 10);
+  alexnet_tiny__decrypt__result0(ctx.get(), ctx->encoder_, ui.get(), evk, encrypted_result, ui.get(), result.data());
 
+  std::cout << "Decrypted result size: " << result.size() << std::endl;
   std::filesystem::path torchPath =
     std::filesystem::path(TEST_DATA_DIR) / "torch_output.bin";
   std::ifstream torch_in(torchPath, std::ios::binary);
