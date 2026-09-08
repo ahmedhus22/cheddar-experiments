d041c5eaa213f725f8bf8b15b6e5729f70a499ad
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2026-09-08 14:49:53 +0000

    temp adjustments to test the belfort labs cheddar backend

--- tests/alexnet-tiny_tests.cpp
+++ tests/alexnet-tiny_tests.cpp
@@ -36,32 +36,45 @@
   using Enc = Encoder<word>;
 
 
-std::vector<Ct> alexnet_tiny(CtxPtr ctx, Enc& encoder, UI& ui, const std::vector<Ct>& v0);
-std::vector<Ct> alexnet_tiny__encrypt__arg0(CtxPtr ctx, Enc& encoder, UI& ui, const std::vector<double>& v0, UI& ui1);
-std::vector<double> alexnet_tiny__decrypt__result0(CtxPtr ctx, Enc& encoder, UI& ui, const std::vector<Ct>& v0, UI& ui1);
-std::tuple<CtxPtr, UI> __configure();
+void alexnet_tiny(Context<word>* v1, const Encoder<word>& v2, const EvaluationKey<word>& v3, const EvkMap<word>& v4, 
+                    const std::array<Ciphertext<word>, 1>& v5, std::array<Ciphertext<word>, 1>& v6);
+void alexnet_tiny__encrypt__arg0(Context<word>* v1, const Encoder<word>& v2, const EvaluationKey<word>& v3, float* v4,
+                                   UserInterface<word>* v5, std::array<Ciphertext<word>, 1>& v6);
+void alexnet_tiny__decrypt__result0(Context<word>* v1, const Encoder<word>& v2, const EvaluationKey<word>& v3,
+                                     const std::array<Ciphertext<word>, 1>& v4, UserInterface<word>* v5, float* v6);
+void alexnet_tiny__setup(std::shared_ptr<Context<word>>& v1);
+void alexnet_tiny__keygen(const std::shared_ptr<Context<word>>& v1, std::unique_ptr<UserInterface<word>>& v2);
+void alexnet_tiny__configure(std::shared_ptr<Context<word>>& v1, std::unique_ptr<UserInterface<word>>& v2);
 
 TEST(CheddarSamples, AlexNetTinyRandom) {
-  auto[ctx, ui] = __configure();
+  std::shared_ptr<cheddar::Context<word>> ctx;
+  std::unique_ptr<UI> ui;
+  alexnet_tiny__configure(ctx, ui);
+
+  ASSERT_NE(ctx, nullptr);
+  ASSERT_NE(ui, nullptr);
+
+  const Evk& evk = ui->GetMultiplicationKey();
+  const EvkMapT& evk_map = ui->GetEvkMap();
 
   std::filesystem::path inPath =
     std::filesystem::path(TEST_DATA_DIR) / "input.bin";
   std::ifstream in(inPath, std::ios::binary);
   std::vector<double> test_input(1 * 3 * 16 * 16);
   in.read(reinterpret_cast<char*>(test_input.data()), test_input.size() * sizeof(double));
+  std::vector<float> test_input_float(test_input.begin(), test_input.end());
 
   // encrypt input
-  auto encrypted_input = alexnet_tiny__encrypt__arg0(ctx, ctx->encoder_, ui, test_input, ui);
+  std::array<Ct, 1> encrypted_input;
+  alexnet_tiny__encrypt__arg0(ctx.get(), ctx->encoder_, evk, &test_input_float[0], ui.get(),
+                       encrypted_input);
   // alexnet_tiny on encrypted input
-  auto encrypted_result = alexnet_tiny(ctx, ctx->encoder_, ui, encrypted_input);
+   std::array<Ciphertext<word>, 1> encrypted_result;
+  alexnet_tiny(ctx.get(), ctx->encoder_, evk, evk_map, encrypted_input, encrypted_result);
   // decrypt result
-  std::vector<double> result = alexnet_tiny__decrypt__result0(ctx, ctx->encoder_, ui, encrypted_result, ui);
-
-  std::filesystem::path outPath =
-    std::filesystem::path(TEST_DATA_DIR) / "cheddar_output.bin";
-  std::ofstream out(outPath, std::ios::binary);
-  out.write(reinterpret_cast<const char*>(result.data()),
-          result.size() * sizeof(double));
+  std::vector<float> result(1 * 10);
+  alexnet_tiny__decrypt__result0(ctx.get(), ctx->encoder_, evk, encrypted_result, ui.get(),
+                          &result[0]);
 
   std::filesystem::path torchPath =
     std::filesystem::path(TEST_DATA_DIR) / "torch_output.bin";
